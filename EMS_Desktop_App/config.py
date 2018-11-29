2cd4053b73954e842476d9c60f55d6adbd0a66d2
cedie712 <cedrick048@yahoo.com>
2018-11-30 04:18:42 +0800

    register models to admin site

--- EMS_Desktop_App/config.py
+++ EMS_Desktop_App/config.py
@@ -1,5 +1,5 @@
 API_HOST = "http://localhost:8000/"
 API_USER = "cedrick"
-API_USER_PASSWORD = "fuckingchangeme"
+API_USER_PASSWORD = "wowowowowow"
 
 CAMERA_INDEX = 0
--- EMS_REST_API_project/EMS_REST_API/admin.py
+++ EMS_REST_API_project/EMS_REST_API/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
+from . models import Employee, EmployeeConfig, EmployeeProfile, AttendanceLog, SalaryReport, GlobalConfig
 
 admin.site.site_header = 'EMS Webext'
 
 # Register your models here.
+admin.site.register(Employee)
+admin.site.register(EmployeeConfig)
+admin.site.register(EmployeeProfile)
+admin.site.register(AttendanceLog)
+admin.site.register(SalaryReport)
+admin.site.register(GlobalConfig)
+
